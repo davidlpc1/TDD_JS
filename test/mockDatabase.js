85d7077127f05c287d8eaedb6f33a932a6637c3c
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-21 17:54:22 -0300

    :sparkles: mock database finished

---
+++ test/mockDatabase.js
@@ -0,0 +1,10 @@
+const mockDatabase = [
+    {
+        name: 'Davi Lucas',
+        age: 14,
+        meta: { revision: 0, created: 1611246982828, version: 0 },
+        '$loki': 1
+    }
+]
+
+module.exports =  mockDatabase
